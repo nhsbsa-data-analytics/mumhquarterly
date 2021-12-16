a160faf399e6c6d7cbf1848ba14945718193e937
GPlatten <32270079+GPlatten@users.noreply.github.com>
2021-12-16 10:55:25 +0000

    Create function to create workbook

---
+++ R/createWB.R
@@ -0,0 +1,46 @@
+#' Easy helper for 'createWB'
+#'
+#' Create base workbook 'wb' and add sheets required
+#' R `%in%` operator.
+#'
+#' @param filepath the filepath to save the workbook to
+#' @param sheets a list of sheet names require
+#' 
+#' @import openxlsx
+#'
+#' @export
+#'
+#' @examples
+#' sheetNames <- c("test1","test2","test3")
+#' createWB("C:/test.xlsx", sheetNames)
+
+createWB <- function(
+  filepath, 
+  sheets
+) {
+  ##create workbook with relevant named tabs
+  wb <- openxlsx::createWorkbook()
+  
+  openxlsx::addWorksheet(wb, sheetName = "Cover_sheet")
+  openxlsx::addWorksheet(wb, sheetName = "Metadata")
+  
+  #remove gridlines on base tabs
+  openxlsx::showGridLines(wb, sheet = "Cover_sheet", showGridLines = FALSE)
+  openxlsx::showGridLines(wb, sheet = "Metadata", showGridLines = FALSE)
+  
+  for(i in 1:length(sheets)) {
+    openxlsx::addWorksheet(wb, sheetName = sheets[i])
+  }
+  
+  #remove gridlines on other tabs
+  for(j in 1:length(sheets)) {
+    openxlsx::showGridLines(wb, sheet = sheets[j], showGridLines = FALSE)
+  }
+  
+  #set font to Arial
+  openxlsx::modifyBaseFont(wb, fontName = "Arial", fontSize = 10)
+  
+  openxlsx::saveWorkbook(wb,
+                         file = filepath,
+                         overwrite = TRUE)
+}
