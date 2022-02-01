f82422a8889f9538bc4fc910fb2dce8df779ad73
GPlatten <32270079+GPlatten@users.noreply.github.com>
2022-02-01 10:21:36 +0000

    create write_sheet function, minor fixes to create_wb (#3)
    * create write_sheet function, minor fixes to create_wb
    
    * Build in option to name workbook and bug fixes
    
    * Fix create_wb example
    
    * Update to create_wb to simplify grid line removal
    
    * Removal of excel file and update to gitignore to skip all excel file types

--- R/create_wb.R
+++ R/create_wb.R
@@ -2,44 +2,40 @@
 #'
 #' Create base workbook 'wb' and add sheets required
 #'
-#' @param filepath the filepath to save the workbook to
 #' @param sheets a list of sheet names require
-#' 
+#'
 #' @import openxlsx
 #'
 #' @export
 #'
 #' @examples
 #' sheetNames <- c("test1","test2","test3")
-#' create_wb("C:/test.xlsx", sheetNames)
+#' create_wb(sheetNames)
 
 create_wb <- function(
-  filepath, 
   sheets
 ) {
-  ##create workbook with relevant named tabs
+  ##create workbook with relevant named sheets
   wb <- openxlsx::createWorkbook()
-  
-  openxlsx::addWorksheet(wb, sheetName = "Cover_sheet")
-  openxlsx::addWorksheet(wb, sheetName = "Metadata")
-  
-  #remove gridlines on base tabs
-  openxlsx::showGridLines(wb, sheet = "Cover_sheet", showGridLines = FALSE)
-  openxlsx::showGridLines(wb, sheet = "Metadata", showGridLines = FALSE)
-  
-  for(i in 1:length(sheets)) {
-    openxlsx::addWorksheet(wb, sheetName = sheets[i])
+
+  openxlsx::addWorksheet(wb,
+                         sheetName = "Cover_sheet",
+                         gridLines = openxlsx_getOp("gridLines", FALSE))
+
+  openxlsx::addWorksheet(wb,
+                         sheetName = "Metadata",
+                         gridLines = openxlsx_getOp("gridLines", FALSE))
+
+
+  for (i in sheets) {
+    openxlsx::addWorksheet(wb,
+                           sheetName = i
+                           gridLines = openxlsx_getOp("gridLines", FALSE))
   }
-  
-  #remove gridlines on other tabs
-  for(j in 1:length(sheets)) {
-    openxlsx::showGridLines(wb, sheet = sheets[j], showGridLines = FALSE)
-  }
-  
+
   #set font to Arial
   openxlsx::modifyBaseFont(wb, fontName = "Arial", fontSize = 10)
-  
-  openxlsx::saveWorkbook(wb,
-                         file = filepath,
-                         overwrite = TRUE)
+
+  #return as object to use in global environment
+  return(wb)
 }
