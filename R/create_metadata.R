3548a62cd3fccafffc3c313764f695de8cee9e19
GPlatten <32270079+GPlatten@users.noreply.github.com>
2022-02-14 17:40:10 +0000

    Create create_metadata.R

---
+++ R/create_metadata.R
@@ -0,0 +1,17 @@
+#' Easy helper for 'create_metadata'
+#'
+#' Create metadata tab for named workbook
+#'
+#' @param fields a list of field names for each item of metadata
+#' @param description a list of descriptions for each item of metadata
+#'
+#' @export
+#'
+#' @examples
+#' sheetNames <- c("test1","test2","test3")
+#' create_wb(sheetNames)
+
+create_metadata <- funtion(
+  fields,
+  descriptions
+)
