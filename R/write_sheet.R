514a51a656a826435942bf9671db60b07c112317
GPlatten <32270079+GPlatten@users.noreply.github.com>
2022-02-17 12:37:02 +0000

    Add auto column widths and ability to manually set width of column A

--- R/write_sheet.R
+++ R/write_sheet.R
@@ -7,6 +7,7 @@
 #' @param title the title of the sheet which will go into cell A1
 #' @param notes a list object of the notes to be included on the sheet
 #' @param dataset the name of the dataset to be written to the names sheet
+#' @param column_a_width the width you would like to assign to column A
 #'
 #' @import openxlsx
 #'
@@ -17,18 +18,20 @@
 #' "test1",
 #' "title1",
 #' c("note1", "note2", "note3", "note4"),
-#' mtcars)
+#' mtcars,
+#' 10)
 
 write_sheet <-  function(workbook,
                          sheetname,
                          title,
                          notes,
-                         dataset) {
-
+                         dataset,
+                         column_a_width) {
+  
   wb <- workbook
-
+  
   notes_list <- notes
-
+  
   #write title
   openxlsx::writeData(
     wb,
@@ -36,7 +39,7 @@ write_sheet <-  function(workbook,
     x = title,
     xy = c(1,1)
   )
-
+  
   #bold title
   openxlsx::addStyle(
     wb,
@@ -45,7 +48,7 @@ write_sheet <-  function(workbook,
     cols = 1,
     rows = 1
   )
-
+  
   #write notes header
   openxlsx::writeData(
     wb,
@@ -53,7 +56,7 @@ write_sheet <-  function(workbook,
     x = "Notes",
     xy = c(1,2)
   )
-
+  
   #bold notes
   openxlsx::addStyle(
     wb,
@@ -62,19 +65,19 @@ write_sheet <-  function(workbook,
     cols = 1,
     rows = 2
   )
-
+  
   #loop to write all notes
   for(i in 1:length(notes_list)) {
-
+    
     openxlsx::writeData(
       wb,
       sheet = sheetname,
       x = notes_list[i],
       xy = c(1,(i + 2))
     )
-
+    
   }
-
+  
   #write data as named data table
   openxlsx::writeDataTable(wb,
                            sheet = sheetname,
@@ -83,21 +86,34 @@ write_sheet <-  function(workbook,
                            tableStyle = "none",
                            withFilter = FALSE,
                            tableName = sheetname)
-
+  
   #set row heights of full range to 14.5 to imporve accessibility
   openxlsx::setRowHeights(wb,
                           sheet = sheetname,
                           rows = c(1:(nrow(dataset) + length(notes_list) + 3)),
                           heights = 14.5)
-
+  
   #create wider rows to use blank space in place of blank rows
   openxlsx::setRowHeights(wb,
                           sheet = sheetname,
                           rows = 2,
                           heights = 29)
-
+  
   openxlsx::setRowHeights(wb,
                           sheet = sheetname,
                           rows = (length(notes_list) + 3),
                           heights = 29)
+  
+  #auto column widths
+  setColWidths(wb, 
+               sheet = sheetname, 
+               cols = 1:ncol(dataset), 
+               widths = "auto")
+  
+  #manually set width of column A to account for title being in A1
+  setColWidths(wb, 
+               sheet = sheetname, 
+               cols = 1, 
+               widths = column_a_width)
 }
+
