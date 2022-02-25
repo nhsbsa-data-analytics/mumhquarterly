b4072bb489f6f7354961b4986de080a65ba75f9f
Matthew Wilson <47149245+MWilson92@users.noreply.github.com>
2022-02-25 13:48:35 +0000

    remove patient model from covid function (#20)
    update description and namespace for package imports

--- R/covid_model.R
+++ R/covid_model.R
@@ -7,10 +7,6 @@
 #'
 #' @param data The data to be input into the model
 #'
-#' @import dplyr
-#' @import tidyr
-#' @import purrr
-#'
 #' @export
 #'
 #' @examples
@@ -22,7 +18,7 @@ covid_model <- function(data) {
   #of items and patients
   df <- data %>%
     dplyr::group_by(SECTION_NAME,SECTION_CODE) %>%
-    tidyr::nest %>%
+    tidyr::nest() %>%
     dplyr::mutate(
       ITEM_MODEL = purrr::map(
         data,
@@ -31,27 +27,16 @@ covid_model <- function(data) {
              data = filter(df, YEAR_MONTH <= 202002))
         }
       ),
-      PATIENT_MODEL = purrr::map(
-        data,
-        function(df) {
-          lm(PATIENT_COUNT ~ MONTH_INDEX + DISPENSING_DAYS + as.factor(MONTH_NUM),
-             data = filter(df, YEAR_MONTH <= 202002))
-        }
-      ),
       PRED_ITEMS_95 = purrr::map2(ITEM_MODEL, data,
                                   ~ as.data.frame(predict(.x, .y, interval = "prediction"))),
-      PRED_PATIENTS_95 = purrr::map2(PATIENT_MODEL, data,
-                                     ~ as.data.frame(predict(.x, .y, interval = "prediction"))),
+
       PRED_ITEMS_90 = purrr::map2(ITEM_MODEL, data,
                                   ~ as.data.frame(predict(.x, .y, interval = "prediction", level = 0.9))),
-      PRED_PATIENTS_90 = purrr::map2(PATIENT_MODEL, data,
-                                     ~ as.data.frame(predict(.x, .y, interval = "prediction", level = 0.9))),
+
       PRED_ITEMS_99 = purrr::map2(ITEM_MODEL, data,
-                                  ~ as.data.frame(predict(.x, .y, interval = "prediction", level = 0.99))),
-      PRED_PATIENTS_99 = purrr::map2(PATIENT_MODEL, data,
-                                     ~ as.data.frame(predict(.x, .y, interval = "prediction", level = 0.99))),
+                                  ~ as.data.frame(predict(.x, .y, interval = "prediction", level = 0.99)))
     ) %>%
-    tidyr::unnest(cols = c(data, PRED_ITEMS_95, PRED_PATIENTS_95, PRED_ITEMS_90, PRED_PATIENTS_90, PRED_ITEMS_99, PRED_PATIENTS_99),
+    tidyr::unnest(cols = c(data, PRED_ITEMS_95, PRED_ITEMS_90, PRED_ITEMS_99),
                   names_repair = "universal",
                   names_sep = "_") %>%
     dplyr::select(-ends_with("_MODEL")) %>%
