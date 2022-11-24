12217677362a7f15c79db0349f0313e6f324c6e4
GPlatten <32270079+GPlatten@users.noreply.github.com>
2022-11-24 14:44:38 +0000

    Fixes to covid_chart_hc to make tooltip order stay constant

--- R/covid_chart_hc.R
+++ R/covid_chart_hc.R
@@ -21,45 +21,57 @@ covid_chart_hc <- function(
 ) {
   chart <- highchart() %>%
     hc_chart(style = list(fontFamily = "Arial")) %>%
-    hc_add_series(data = data,
-                  name = "99% prediction interval",
-                  type = "arearange",
-                  lineWidth = 0,
-                  color = rgb(66,85,99, alpha = 70, maxColorValue = 255),
-                  marker = list(enabled = FALSE),
-                  dataLabels = list(enabled = FALSE),
-                  # enableMouseTracking = FALSE,
-                  hcaes(x = MONTH_START,
-                        high = signif(PRED_ITEMS_99_UPR,3),
-                        low = signif(PRED_ITEMS_99_LWR,3))) %>%
-    hc_add_series(data = data,
-                  name = "95% prediction interval",
-                  type = "arearange",
-                  lineWidth = 0,
-                  color = rgb(66,85,99, alpha = 140, maxColorValue = 255),
-                  marker = list(enabled = FALSE),
-                  dataLabels = list(enabled = FALSE),
-                  hcaes(x = MONTH_START,
-                        high = signif(PRED_ITEMS_95_UPR,3),
-                        low = signif(PRED_ITEMS_95_LWR,3))) %>%
-    hc_add_series(data = data,
-                  name = "Expected items",
-                  type = "line",
-                  dashStyle = "Dash",
-                  color = "#231f20",
-                  marker = list(enabled = FALSE),
-                  dataLabels = list(enabled = FALSE),
-                  hcaes(x = MONTH_START,
-                        y = signif(PRED_ITEMS_95_FIT,3))) %>%
-    hc_add_series(data = data,
-                  name = "Prescribed items",
-                  type = "line",
-                  lineWidth = 3,
-                  color = "#005EB8",
-                  marker = list(enabled = FALSE),
-                  dataLabels = list(enabled = FALSE),
-                  hcaes(x = MONTH_START,
-                        y = signif(ITEM_COUNT,3))) %>%
+    hc_add_series(
+      data = data,
+      name = "99% prediction interval",
+      type = "arearange",
+      lineWidth = 0,
+      color = rgb(66, 85, 99, alpha = 70, maxColorValue = 255),
+      marker = list(enabled = FALSE),
+      dataLabels = list(enabled = FALSE),
+      # enableMouseTracking = FALSE,
+      hcaes(
+        x = MONTH_START,
+        high = signif(PRED_ITEMS_99_UPR, 3),
+        low = signif(PRED_ITEMS_99_LWR, 3)
+      )
+    ) %>%
+    hc_add_series(
+      data = data,
+      name = "95% prediction interval",
+      type = "arearange",
+      lineWidth = 0,
+      color = rgb(66, 85, 99, alpha = 140, maxColorValue = 255),
+      marker = list(enabled = FALSE),
+      dataLabels = list(enabled = FALSE),
+      hcaes(
+        x = MONTH_START,
+        high = signif(PRED_ITEMS_95_UPR, 3),
+        low = signif(PRED_ITEMS_95_LWR, 3)
+      )
+    ) %>%
+    hc_add_series(
+      data = data,
+      name = "Expected items",
+      type = "line",
+      dashStyle = "Dash",
+      color = "#231f20",
+      marker = list(enabled = FALSE),
+      dataLabels = list(enabled = FALSE),
+      hcaes(x = MONTH_START,
+            y = signif(PRED_ITEMS_95_FIT, 3))
+    ) %>%
+    hc_add_series(
+      data = data,
+      name = "Prescribed items",
+      type = "line",
+      lineWidth = 3,
+      color = "#005EB8",
+      marker = list(enabled = FALSE),
+      dataLabels = list(enabled = FALSE),
+      hcaes(x = MONTH_START,
+            y = signif(ITEM_COUNT, 3))
+    ) %>%
     hc_xAxis(type = "datetime",
              dateTimeLabelFormats = list(month = "%b %y"),
              title = list(text = "Month")) %>%
@@ -68,10 +80,31 @@ covid_chart_hc <- function(
     hc_title(text = title,
              style = list(fontSize = "16px",
                           fontWeight = "bold")) %>%
-    hc_legend(enabled = TRUE) %>%
-    hc_tooltip(enabled = TRUE,
-               shared = TRUE,
-               sort = TRUE) %>%
+    hc_legend(enabled = TRUE,
+              reversed = TRUE) %>%
+    hc_tooltip(
+      enabled = TRUE,
+      shared = TRUE,
+      useHTML = TRUE,
+      formatter = JS(
+        "function () {
+        var timeStamp = this.x;
+        var dateFormat = new Date(timeStamp);
+        var month = dateFormat.toLocaleString('default', { month: 'long' });
+        var year = dateFormat.getFullYear();
+
+        var s = month + ' ' + year;
+
+        $.each(this.points.reverse(), function () {
+            var number = new Number(this.y).toLocaleString('en-US', {minimumFractionDigits: 0});
+            s += '<br/><span style=\"color:' + this.series.color + '\">\u25CF</span> ' + this.series.name + ': ' +
+                '<b>' + number + '</b>';
+        });
+
+        return s;
+    }"
+      )
+    ) %>%
     hc_credits(enabled = TRUE) %>%
     hc_plotOptions(arearange = list(states = list(hover = list(enabled = FALSE))))
 
